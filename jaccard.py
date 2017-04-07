2d9f493b42cecf6cae682172c450f38b92e0f8fd
adityatrilok90 <adityatrilok90@gmail.com>
2017-04-06 20:30:38 -0500

    Final jaccard.py file
    A Python program to find the Jaccard similarity between the (question & the best answer) and (question & all answers). The answer with the highest Jaccard Index is considered as the best answer.(Jaccard index is the intersection of the two sets/ Union of the two sets)

---
+++ jaccard.py
@@ -0,0 +1,37 @@
+"""
+Created on Thu Apr 28 11:27:55 2016
+
+@author: aditya kashyap
+"""
+
+import xml.etree.ElementTree as ET
+import operator
+from functools import reduce
+import numpy as np
+import itertools
+from collections import Counter
+import string
+import nltk
+
+
+def get_similarity(element):
+
+    if element[2] == None:
+        question = element[1]
+    else:
+        question = (element[1] + element[2])
+    jaccardlist = []
+    j = [question] + element[3:]
+    set_x = set(j)
+    for i in range(3,len(element)):
+        k = question + element[i]
+        set_y = set(k)
+        numerator = len(set_x.intersection(set_y)) #length of intersection of x and y
+        denominator = float(len(set_x.union(set_y)))
+        jaccardlist.append(numerator / denominator)
+    m = jaccardlist.index(max(jaccardlist))
+    m = m + 3
+    if element[0] == element[m]:
+        return 1
+    else:
+        return 0
